61c7ea16a42f1ff165d471eef8275b292a8be83a
Pedro Fontes <pedro.1note@outlook.com>
2026-04-15 20:32:55 -0300

    Tela para ler QRCode; Construção de outros componentes (backbutton); melhorias e etc

--- mempresta/app/_layout.tsx
+++ mempresta/app/_layout.tsx
@@ -11,25 +11,11 @@ export default function RootLayout() {
         name="homepageu"
         options={{ headerShown: false }}
       />
+      <Stack.Screen
+        name="homepageg"
+        options={{ headerShown: false }}
+      />
 
     </Stack>
   );
 }
-
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import LoginScreen from './index';
-// import ProfileScreen from './';
-
-// const Stack = createNativeStackNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen name="Login" component={LoginScreen} />
-//         <Stack.Screen name="Profile" component={ProfileScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
--- mempresta/app/index.tsx
+++ mempresta/app/index.tsx
@@ -1,7 +1,10 @@
 import {
   Pressable,
   Text,
-  View
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView
 } from 'react-native';
 
 import { Input } from '@/components/inputs';
@@ -15,17 +18,18 @@ import { useState } from 'react';
 export default function LoginScreen() {
   const [nome, setNome] = useState('');
   const [senha, setSenha] = useState('');
+
   const router = useRouter();
+
   const handleEnviar = () => {
-    if ((nome === 'pedro' || nome === "yan") && senha === '123') {
+    if ((nome === 'pedro') && senha === '123') {
       console.log('Login bem-sucedido!');
-      alert('Login bem-sucedido!'); 
+      alert('Login bem-sucedido!');
       router.push('/homepageu');
-
-      // redirecionar para a próxima tela ou realizar outras ações necessárias
-      // Exemplo: navigation.navigate('Home');
-      //navigation.navigate('homepageu');
-
+    } else if ((nome === 'yan') && senha === '123') {
+      console.log('Login bem-sucedido!');
+      alert('Login bem-sucedido!');
+      router.push('/homepageg');
     } else {
       console.log('Credenciais inválidas. Tente novamente.');
       alert('Credenciais inválidas. Tente novamente.');
@@ -34,33 +38,39 @@ export default function LoginScreen() {
 
   return (
     // Container principal da tela de login
-    <View style={styles.container} >
-      <Perfil width={250} height={250} />
-      <Input placeholder='Usuário' setfunction={setNome} />
-      <Input placeholder='Senha' setfunction={setSenha} />
-      <Pressable style={styles.button} onPress={handleEnviar}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </Pressable>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <ScrollView contentContainerStyle={styles.container}>
 
-    // Container para os links de cadastro e recuperação de senha
-      <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 20 }}>
-        <View style={{ flexDirection: 'row', marginTop: 5 }}>
-          <Text>
-            Não tem uma conta? {' '}
-            <Link href="/homepageu" style={{ color: '#b20fc8' }}>
-              Cadastre-se
-            </Link>
-          </Text>
+        <View style={styles.container} >
+          <Perfil width={250} height={250} />
+          <Input placeholder='Usuário' setfunction={setNome} />
+          <Input placeholder='Senha' setfunction={setSenha} />
+          <Pressable style={styles.button} onPress={handleEnviar}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </Pressable>
+
+          <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 20 }}>
+            <View style={{ flexDirection: 'row', marginTop: 5 }}>
+              <Text>
+                Não tem uma conta? {' '}
+                <Link href="/homepageu" style={{ color: '#b20fc8' }}>
+                  Cadastre-se
+                </Link>
+              </Text>
+            </View>
+            <View style={{ flexDirection: 'row', marginTop: 5 }}>
+              <Text>
+                Esqueceu a senha? {' '}
+                <Link href="../app/web/index.html" style={{ color: '#b20fc8' }}>
+                  Recuperar Senha
+                </Link>
+              </Text>
+            </View>
+          </View>
         </View>
-        <View style={{ flexDirection: 'row', marginTop: 5 }}>
-          <Text>
-            Esqueceu a senha? {' '}
-            <Link href="https://example.com/forgot-password" style={{ color: '#b20fc8' }}>
-              Recuperar Senha
-            </Link>
-          </Text>
-        </View>
-      </View>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
